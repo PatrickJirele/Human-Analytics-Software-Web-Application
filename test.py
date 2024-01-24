4cd0a84b683f2fdca765a2ee2d7779d87ef21485
Natetodd55 <nathantodd55@gmail.com>
2024-01-24 09:56:25 -0700

    static, templates, home.html

--- test.py
+++ test.py
@@ -4,3 +4,13 @@ import re
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
+
+app = Flask(__name__, static_url_path='/static')
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
