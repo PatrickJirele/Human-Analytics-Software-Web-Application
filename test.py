84d4eacca05c7a940239321961213be33f89da53
Natetodd55 <nathantodd55@gmail.com>
2024-02-02 08:47:17 -0700

    Prototype v2
    changed forms and added date conversion to years at western

--- test.py
+++ test.py
@@ -4,6 +4,18 @@ import re
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
+import pandas as pd
+from datetime import datetime
+
+
+#df = pd.read_csv('tempFileName.csv')
+def hireDateConversion():
+    df['Hire Date'] = pd.to_datetime(df['Hire Date'])
+    current_date = datetime.now()
+    df['Years Since Hire'] = (current_date - df['Hire Date']).dt.days / 365.25
+    print(df)
+
+
 
 app = Flask(__name__, static_url_path='/static')
 
